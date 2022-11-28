9790d02dad3c83c655508167ee813836d0731f4c
f4faysal <f4faysals@gmai.com.com>
2022-11-29 02:05:55 +0600

    add a admin condition

--- src/page/Login/Login.js
+++ src/page/Login/Login.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import toast from "react-hot-toast";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import { setAuthToken } from "../../api/auth";
