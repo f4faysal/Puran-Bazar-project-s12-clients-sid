bddeea76880f556f1dc95d244266523ca30c94e4
f4faysal <f4faysals@gmai.com.com>
2022-11-28 21:56:31 +0600

    add a advatise option

--- src/page/Home/Adverties/Adverties.js
+++ src/page/Home/Adverties/Adverties.js
@@ -1,36 +1,68 @@
-import React from "react";
+import { useQuery } from "@tanstack/react-query";
+import React, { useContext, useState } from "react";
+import ItemCard from "../../../Components/Card/ItemCard";
+import SmallSpinner from "../../../Components/Spinner/SmallSpinner";
+import { AuthContext } from "../../../contexts/AuthProvider";
+import ItemModal from "../Categories/CategoriseItem/ItemModal";
 
 const Adverties = () => {
+  const { user } = useContext(AuthContext);
+  const [bookingItem, srtBookingItem] = useState(null);
 
+  const {
+    data: advatices = [],
+    refetch,
+    isLoading,
+  } = useQuery({
+    queryKey: ["advatices"],
+    queryFn: async () => {
+      try {
+        const url = `http://localhost:5000/advatices/${user.email}`;
+        // try cghat function handel to error
+        const res = await fetch(
+          url
+          //     {
+          //   headers: {
+          //     authorization: `bearer ${localStorage.getItem("access-token")}`,
+          //   },
+          // }
+        );
+        const data = await res.json();
+        console.log("Adverties", data);
+        return data;
+      } catch (error) {
+        console.log("errorerror", error);
+      }
+    },
+  });
 
-    
+  if(isLoading){
+    return <SmallSpinner></SmallSpinner>
+  }
 
+  console.log("data", advatices);
   return (
     <div className="mb-5 mt-5">
       <div className="hero  rounded p-5">
         <div className="hero-content flex-col lg:flex-row-reverse lg:gap-20 ">
-          <div className="grid grid-cols-1  md:grid-cols-3 lg:grid-cols-5  items-center gap-4 justify-center justify-items-center">
-            {/* {products?.map((item) => (
+          { user ? <div className="grid grid-cols-1  md:grid-cols-3 lg:grid-cols-5  items-center gap-4 justify-center justify-items-center">
+            {advatices?.map((item) => (
               <ItemCard
                 key={item._id}
                 products={item}
                 srtSookingItem={srtBookingItem}
               ></ItemCard>
-            ))} */}
-            <div className="card w-96 bg-base-100 shadow-xl">
-              <figure>
-                <img src="https://placeimg.com/400/225/arch" alt="Shoes" />
-              </figure>
-              <div className="card-body">
-                <h2 className="card-title">Shoes!</h2>
-                <p>If a dog chews shoes whose shoes does he choose?</p>
-                <div className="card-actions justify-end">
-                  <button className="btn btn-primary">Buy Now</button>
-                </div>
-              </div>
-            </div>
-            
-          </div>
+            ))}
+          
+          </div> :<SmallSpinner></SmallSpinner> }
+
+          {bookingItem && (
+        <ItemModal
+          bookingItem={bookingItem}
+          refetch={refetch}
+          srtBookingItem={srtBookingItem}
+        ></ItemModal>
+      )}
         </div>
       </div>
     </div>
